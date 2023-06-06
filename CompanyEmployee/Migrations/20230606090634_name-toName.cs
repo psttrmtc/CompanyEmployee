cb76cbf887d65bd550b254c2a529b32c3549f61d
minhloi12300@gmail.com <minhloi12300@gmail.com>
2023-06-06 17:52:44 +0700

    Add project files.

---
+++ CompanyEmployee/Migrations/20230606090634_name-toName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CompanyEmployee.Migrations
+{
+    public partial class nametoName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "Companies",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Companies",
+                newName: "name");
+        }
+    }
+}
